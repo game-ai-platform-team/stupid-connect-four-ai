73c5570231c89c712ce3f7ca77b8c195beadb9a3
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-20 12:49:54 +0200

    fix: Only output random moves

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-from connect_four_lib.connect_four_engine import ConnectFourEngine
-from connect_four_lib.connect_four_judge import ConnectFourJudge
-from connect_four_lib.game_state import GameState
+import random
 
 if __name__ == "__main__":
-    judge = ConnectFourJudge()
-    engine = ConnectFourEngine(judge=judge)
+    while True:
+        move = random.choice(range(7))
 
-    game = ConnectFourEngine()
+        print("MOVE: " + str(move))
 
-    while judge.is_game_over() == GameState.CONTINUE:
-        try:
-            move = engine.get_random_move()
-            engine.add_move(move)
-
-            print("MOVE: " + str(move))
-
-            opponent_move = input()
-
-            if opponent_move != "":
-                engine.add_move(opponent_move)
-
-        except ValueError:
-            print("Invalid input. Please enter a valid column number.")
+        opponent_move = input()
