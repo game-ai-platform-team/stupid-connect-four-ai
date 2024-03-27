035748fc4b03ab7a3d054258772c00c1e7c51af2
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-03-27 17:01:21 +0200

    refactor: updated to new protocol

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import random
 
 if __name__ == "__main__":
+    count = {i: 0 for i in range(7)}
+
     while True:
-        move = random.choice(range(7))
+        read = input().split(":")
+        print(f"Read input: {read}")
+        tag = read[0]
+        print(f"Columns: {count}")
+        if tag == "PLAY":
+            while True:
+                num = random.randint(0, 6)
+                if count[num] < 6:
+                    print(f"MOVE:{num}")
 
-        print("MOVE: " + str(move))
-
-        opponent_move = input()
+                    count[num] += 1
+                    break
+                else:
+                    print(f"Random result was {num}, but there was no space!")
+        elif tag == "MOVE":
+            op_move = read[1]
+            count[int(op_move)] += 1
+        elif tag == "RESET":
+            count = {i: 0 for i in range(7)}
