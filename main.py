180daa6070cd92950daea184d5838284b3b3c09d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-23 15:18:15 +0300

    refactor: Tests number from left
    Co-authored-by: Miika Piiparinen <MiikaMatias@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-
 if __name__ == "__main__":
-    count = {i: 0 for i in range(7)}
+    count = [0] * 7
 
     while True:
         read = input().split(":")
@@ -9,25 +7,29 @@ if __name__ == "__main__":
         tag = read[0]
         data = read[1]
         print(f"Columns: {count}")
+
         if tag == "PLAY":
             print("finding move...")
-            while True:
-                num = random.randint(0, 6)
-                if count[num] < 6:
-                    print(f"MOVE:{num}")
 
-                    count[num] += 1
+            for i, value in enumerate(count):
+                if value < 6:
+                    print(f"MOVE:{i}")
+                    count[i] += 1
                     break
-                else:
-                    print(f"Random result was {num}, but there was no space!")
+
+            print("MOVE: -1")
+            print("Error: No moves left!")
+
         elif tag == "MOVE":
             op_move = read[1]
             count[int(op_move)] += 1
+
         elif tag == "BOARD":
             count = {i: 0 for i in range(7)}
             if len(data) > 0:
                 for i in data.split(","):
                     count[int(i)] += 1
             print(f"Board set to: {count}")
+
         elif tag == "RESET":
             count = {i: 0 for i in range(7)}
